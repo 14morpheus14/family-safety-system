6d4bc2c58ee74972598960b872694ba39703ff96
Aagnik Chakraborty <97616804+godofshock@users.noreply.github.com>
2026-05-07 07:43:02 +0000

    make razorpay initialization optional

--- backend/src/services/razorpay.service.ts
+++ backend/src/services/razorpay.service.ts
@@ -1,9 +1,15 @@
 import Razorpay from "razorpay";
-import env from "../config/env";
 
-const razorpay = new Razorpay({
-  key_id: env.RAZORPAY_KEY_ID || "",
-  key_secret: env.RAZORPAY_KEY_SECRET || ""
-});
+let razorpayInstance: Razorpay | null = null;
 
-export default razorpay;
+if (
+  process.env.RAZORPAY_KEY_ID &&
+  process.env.RAZORPAY_KEY_SECRET
+) {
+  razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+}
+
+export default razorpayInstance;
